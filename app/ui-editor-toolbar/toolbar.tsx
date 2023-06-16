492415051f62ac152daf02088320d363e844eccb
youking-lib <whistleyz@163.com>
2023-06-17 01:37:47 +0800

    fix: react component validate

--- app/ui-editor-toolbar/toolbar.tsx
+++ app/ui-editor-toolbar/toolbar.tsx
@@ -83,6 +83,7 @@ export function InlineToolBtn() {
           <Toolbar.ToggleItem
             className="flex-shrink-0 flex-grow-0 basis-auto text-mauve11 h-[25px] px-[5px] rounded inline-flex text-[13px] leading-none items-center justify-center  ml-0.5 outline-none hover:bg-violet3 hover:text-violet11 focus:relative focus:shadow-[0_0_0_2px] focus:shadow-violet7 first:ml-0 data-[state=on]:bg-secondary data-[state=on]:text-secondary-content"
             value={tool.key}
+            key={tool.key}
             aria-label={tool.key}
             onClick={tool.toggle}
           >
--- app/ui-toast/index.tsx
+++ app/ui-toast/index.tsx
@@ -29,3 +29,5 @@ export const Toast = React.forwardRef<ToastRef, ToastProps>(
     );
   }
 );
+
+Toast.displayName = "ui-toast";
